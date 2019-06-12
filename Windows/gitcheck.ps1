9d256a31d4c6b1fd2b379e535a4d42b9ee0eb851
philanderson888 <philanderson888@hotmail.com>
2019-06-12 14:36:07 +0100

    updating gitpush

--- Windows/gitcheck.ps1
+++ Windows/gitcheck.ps1
@@ -2,51 +2,52 @@
 copy -Force C:\Users\TECH-W99a\WindowsPowerShell\gitpush.ps1 C:\scripts\Windows
 copy -Force C:\Users\TECH-W99a\WindowsPowerShell\gitsetup.ps1 C:\scripts\Windows
 copy -Force C:\Users\TECH-W99a\WindowsPowerShell\test.ps1 C:\scripts\Windows
-echo ""
-echo ""
-echo 2019-04-c-sharp-labs
-cd C:\2019-04-c-sharp-labs
-git pull
-git status
-echo ""
-echo ""
-echo bootstrap
-cd C:\bootstrap
-git pull
-git status
-echo ""
-echo ""
-echo c-sharp
-cd C:\c-sharp
-git pull
-git status
-echo ""
-echo ""
-echo cyber
-cd C:\cyber
-git pull
-git status
-echo ""
-echo ""
-echo scripts
-cd C:\scripts
-git pull
-git status
-echo ""
-echo ""
-echo vue
-cd c:\vue
-git pull
-git status
-echo ""
-echo ""
-echo closed-source-sparta-notes
-cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\closed-source-sparta-notes'
-git pull
-git status
-echo ""
-echo ""
-echo open-source-notes
-cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\open-source-notes'
-git pull
-git status
+echo "" > c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo 2019-04-c-sharp-labs >> c:/data/output.txt
+cd C:\2019-04-c-sharp-labs >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo bootstrap >> c:/data/output.txt
+cd C:\bootstrap >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo c-sharp >> c:/data/output.txt
+cd C:\c-sharp >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo cyber >> c:/data/output.txt
+cd C:\cyber >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo scripts >> c:/data/output.txt
+cd C:\scripts >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo vue >> c:/data/output.txt
+cd c:\vue >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo closed-source-sparta-notes >> c:/data/output.txt
+cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\closed-source-sparta-notes' >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo open-source-notes >> c:/data/output.txt
+cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\open-source-notes' >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+subl c:/data/output.txt
