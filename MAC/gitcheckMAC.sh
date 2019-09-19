67f85fb0a43ee9f5aedb1ba10b26ff4e601aaa69
philanderson888 <philanderson888@hotmail.com>
2019-09-19 06:40:56 +0100

    Updating scripts

--- MAC/gitcheckMAC.sh
+++ MAC/gitcheckMAC.sh
@@ -1,4 +1,4 @@
-echo "trying to push to github with a script"
+echo "Updating scripts"
 cd /Users/Shared/scripts
 git status
 git pull
@@ -11,3 +11,44 @@ git status
 git pull
 git add .
 git commit -m "Updating Showcase"
+git push
+
+echo "learn-graph-ql"
+cd /Users/Shared/learn-graph-ql
+git status
+git pull
+git add .
+git commit -m "Updating learn-graph-ql"
+git push
+
+echo "react"
+cd /Users/Shared/react
+git status
+git pull
+git add .
+git commit -m "Updating React"
+git push
+
+echo "Updating vue"
+cd /Users/Shared/vue
+git status
+git pull
+git add .
+git commit -m "Updating Vue"
+git push
+
+echo ""
+cd /Users/Shared/
+git status
+git pull
+git add .
+git commit -m "Updating"
+git push
+
+echo "vue-firebase-app"
+cd /Users/Shared/vue-firebase-app
+git status
+git pull
+git add .
+git commit -m "Updating vue-firebase-app"
+git push
