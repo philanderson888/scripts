4e83d38c05c87fee69344bc8192b75e52e347c39
philanderson888 <philanderson888@hotmail.com>
2019-10-17 16:01:46 +0100

    updating scripts in Windows

--- Windows/gitcheck.ps1
+++ Windows/gitcheck.ps1
@@ -6,12 +6,6 @@ copy -Force C:\Users\TECH-W99a\WindowsPowerShell\test.ps1 C:\scripts\Windows
 echo "" > c:/data/output.txt
 echo "" >> c:/data/output.txt
 
-echo 2019-06-c-sharp-labs >> c:/data/output.txt
-cd C:\2019-06-c-sharp-labs >> c:/data/output.txt
-git pull >> c:/data/output.txt
-git status >> c:/data/output.txt
-echo "" >> c:/data/output.txt
-echo "" >> c:/data/output.txt
 
 echo 2019-04-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-04-c-sharp-labs >> c:/data/output.txt
@@ -19,6 +13,25 @@ git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
+
+
+
+echo 2019-06-c-sharp-labs >> c:/data/output.txt
+cd C:\2019-06-c-sharp-labs >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+echo 2019-09-c-sharp-labs >> c:/data/output.txt
+cd C:\2019-09-c-sharp-labs >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
 
 echo bootstrap >> c:/data/output.txt
 cd C:\bootstrap >> c:/data/output.txt
@@ -41,12 +54,42 @@ git status >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
+
+
+
+echo gatsby-coffee >> c:/data/output.txt
+cd C:\gatsby-coffee >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+echo learn-graph-ql >> c:/data/output.txt
+cd C:\learn-graph-ql >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
 echo scripts >> c:/data/output.txt
 cd C:\scripts >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
+
+
+
+echo showcase >> c:/data/output.txt
+cd C:\showcase >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
 
 echo vue >> c:/data/output.txt
 cd c:\vue >> c:/data/output.txt
@@ -62,6 +105,8 @@ git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
+
+
 
 echo open-source-notes >> c:/data/output.txt
 cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\open-source-notes' >> c:/data/output.txt
