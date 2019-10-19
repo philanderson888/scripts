efd7da3e84726492a72d1c9da6f033aad622ed5a
philanderson888 <philanderson888@hotmail.com>
2019-10-19 12:28:24 +0100

    updating gitcheckMAC

--- MAC/gitcheckMAC.sh
+++ MAC/gitcheckMAC.sh
@@ -5,6 +5,8 @@ git pull
 git add .
 git commit -m "Updating scripts"
 git push
+
+
 echo "Showcase"
 cd /Users/Shared/showcase
 git status
@@ -12,6 +14,8 @@ git pull
 git add .
 git commit -m "Updating Showcase"
 git push
+
+
 
 echo "learn-graph-ql"
 cd /Users/Shared/learn-graph-ql
@@ -21,6 +25,8 @@ git add .
 git commit -m "Updating learn-graph-ql"
 git push
 
+
+
 echo "react"
 cd /Users/Shared/react
 git status
@@ -28,6 +34,8 @@ git pull
 git add .
 git commit -m "Updating React"
 git push
+
+
 
 echo "Updating vue"
 cd /Users/Shared/vue
@@ -37,18 +45,5 @@ git add .
 git commit -m "Updating Vue"
 git push
 
-echo ""
-cd /Users/Shared/
-git status
-git pull
-git add .
-git commit -m "Updating"
-git push
 
-echo "vue-firebase-app"
-cd /Users/Shared/vue-firebase-app
-git status
-git pull
-git add .
-git commit -m "Updating vue-firebase-app"
-git push
+
