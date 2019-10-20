8d69e62ebbf9f47f9bb08f249deb749a1be415c4
philanderson888 <philanderson888@hotmail.com>
2019-10-20 06:23:35 +0100

    adding script for windows on mac

---
+++ Windows/gitcheckapple.ps1
@@ -0,0 +1,33 @@
+﻿copy -Force C:\Users\apple\WindowsPowerShell\gitcheck.ps1 C:\scripts\Windows
+copy -Force C:\Users\apple\WindowsPowerShell\gitcheckapple.ps1 C:\scripts\Windows
+copy -Force C:\Users\apple\WindowsPowerShell\gitpush.ps1 C:\scripts\Windows
+copy -Force C:\Users\apple\WindowsPowerShell\gitsetup.ps1 C:\scripts\Windows
+copy -Force C:\Users\apple\WindowsPowerShell\test.ps1 C:\scripts\Windows
+
+echo "" > c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+
+echo c-sharp >> c:/data/output.txt
+cd C:\c-sharp >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+
+
+
+
+echo scripts >> c:/data/output.txt
+cd C:\scripts >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+
+notepad C:\data\output.txt
