48b978fa183464cabda2c01ef75f1b5bbbfe89ea
Phil <philanderson888@hotmail.com>
2019-10-28 10:49:17 +0000

    Updating scripts

--- Windows/gitcheck.ps1
+++ Windows/gitcheck.ps1
@@ -15,7 +15,7 @@ echo "" >> c:/data/output.txt
 
 
 
-echo 2019-04-c-sharp-labs ===== 
+echo " " " " 2019-04-c-sharp-labs 
 echo 2019-04-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-04-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
@@ -29,7 +29,7 @@ echo "" >> c:/data/output.txt
 
 
 
-echo 2019-06-c-sharp-labs ===== 
+echo " " " " 2019-06-c-sharp-labs
 echo 2019-06-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-06-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
@@ -46,7 +46,7 @@ echo "" >> c:/data/output.txt
 
 
 
-echo 2019-09-c-sharp-labs ===== 
+echo " " " " 2019-09-c-sharp-labs 
 echo 2019-09-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-09-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
@@ -62,11 +62,11 @@ echo "" >> c:/data/output.txt
 
 
 
-echo bootstrap ===== 
+echo " " " " bootstrap
 echo bootstrap >> c:/data/output.txt
 cd C:\bootstrap >> c:/data/output.txt
 git pull >> c:/data/output.txt
-git status >> c:/data/output.txt
+git status >> c:/data" " " " /output.txt
 git add . >> c:/data/output.txt
 git commit -m "Updating bootstrap" >> c:/data/output.txt
 git push >> c:/data/output.txt
@@ -75,9 +75,9 @@ echo "" >> c:/data/output.txt
 
 
 
-echo c-sharp ===== 
+echo " " " " c-sharp
 echo c-sharp >> c:/data/output.txt
-cd C:\c-sharp >> c:/data/output.txt
+cd C:\c-sharp >> c:/data/output.txt" " " " 
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
 git add . >> c:/data/output.txt
@@ -87,7 +87,8 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
-
+echo ""
+echo ""
 echo course-notes ===== 
 echo course-notes >> c:/data/output.txt
 cd C:\GDrive\Notes\course-notes >> c:/data/output.txt
@@ -100,7 +101,8 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
-
+echo ""
+echo ""
 echo cyber  ===== 
 echo cyber >> c:/data/output.txt
 cd C:\cyber >> c:/data/output.txt
@@ -113,7 +115,8 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
-
+echo ""
+echo ""
 echo gatsby-coffee ===== 
 echo gatsby-coffee >> c:/data/output.txt
 cd C:\gatsby-coffee >> c:/data/output.txt
@@ -126,7 +129,8 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
-
+echo ""
+echo ""
 echo learn-graph-ql ===== 
 echo learn-graph-ql >> c:/data/output.txt
 cd C:\learn-graph-ql >> c:/data/output.txt
@@ -138,6 +142,9 @@ git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
+
+echo ""
+echo ""
 echo scripts  ===== 
 echo scripts >> c:/data/output.txt
 cd C:\scripts >> c:/data/output.txt
@@ -150,6 +157,9 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
+
+echo ""
+echo ""
 echo showcase ===== 
 echo showcase >> c:/data/output.txt
 cd C:\showcase >> c:/data/output.txt
@@ -162,6 +172,9 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
+
+echo ""
+echo ""
 echo vue ===== 
 echo vue >> c:/data/output.txt
 cd c:\vue >> c:/data/output.txt
@@ -176,7 +189,8 @@ echo "" >> c:/data/output.txt
 
 
 
-
+echo ""
+echo ""
 echo "Not doing - C:\vue-with-paypal-payment - Updating vue-with-paypal-payment"
 
 
@@ -184,7 +198,8 @@ echo "Not doing - C:\vue-with-paypal-payment - Updating vue-with-paypal-payment"
 
 
 
-
+echo ""
+echo ""
 echo closed-source-sparta-notes  ===== 
 echo closed-source-sparta-notes >> c:/data/output.txt
 cd "C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\closed-source-sparta-notes" >> c:/data/output.txt
@@ -197,6 +212,11 @@ echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
 
+
+
+
+echo ""
+echo ""
 echo open-source-notes ===== 
 echo open-source-notes >> c:/data/output.txt
 cd 'C:\GDrive\Notes\course-notes' >> c:/data/output.txt
