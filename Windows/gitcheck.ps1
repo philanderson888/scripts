345eaf18f777f23c9e80f9d1b228bd5fc4bc382a
Phil <philanderson888@hotmail.com>
2019-10-28 10:10:08 +0000

    Updating scripts

--- Windows/gitcheck.ps1
+++ Windows/gitcheck.ps1
@@ -12,6 +12,9 @@ echo 2019-04-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-04-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add .
+git commit -m "Updating 2019-04-labs"
+git push
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -20,6 +23,9 @@ echo 2019-06-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-06-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add .
+git commit -m "Updating 2019-06-labs"
+git push
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -28,6 +34,9 @@ echo 2019-09-c-sharp-labs >> c:/data/output.txt
 cd C:\2019-09-c-sharp-labs >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating 2019-09-labs" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -37,6 +46,9 @@ echo bootstrap >> c:/data/output.txt
 cd C:\bootstrap >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating bootstrap" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -47,6 +59,9 @@ echo c-sharp >> c:/data/output.txt
 cd C:\c-sharp >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating c-sharp" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -55,9 +70,9 @@ echo course-notes >> c:/data/output.txt
 cd C:\GDrive\Notes\course-notes >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating course notes"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating course notes" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -68,9 +83,9 @@ echo cyber >> c:/data/output.txt
 cd C:\cyber >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating cyber notes"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating cyber notes" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -81,6 +96,9 @@ echo gatsby-coffee >> c:/data/output.txt
 cd C:\gatsby-coffee >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating gatsby-coffee" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -89,9 +107,9 @@ echo learn-graph-ql >> c:/data/output.txt
 cd C:\learn-graph-ql >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating learn-graph-ql"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating learn-graph-ql" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -100,9 +118,9 @@ echo scripts >> c:/data/output.txt
 cd C:\scripts >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating scripts"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating scripts" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -112,9 +130,9 @@ echo showcase >> c:/data/output.txt
 cd C:\showcase >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating showcase"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating showcase" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -124,9 +142,9 @@ echo vue >> c:/data/output.txt
 cd c:\vue >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating vue"
-git push
+git add . >> c:/data/output.txt
+git commit -m "Updating vue" >> c:/data/output.txt
+git push >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 echo "" >> c:/data/output.txt
 
@@ -135,13 +153,14 @@ echo "" >> c:/data/output.txt
 
 
 echo "Updating vue-with-paypal-payment"
-cd C:\vue-with-paypal-payment
-git status
-git pull
-git add .
-git commit -m "Updating Vue With Paypal Payment"
-git push
-echo "";
+cd C:\vue-with-paypal-payment >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating Vue With Paypal Payment" >> c:/data/output.txt
+git push >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
 
 
 
@@ -152,16 +171,26 @@ echo closed-source-sparta-notes >> c:/data/output.txt
 cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\closed-source-sparta-notes' >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-git add .
-git commit -m "Updating closed source sparta notes"
-git push
-echo "" >> c:/data/output.txt
-echo "" >> c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating closed source sparta notes" >> c:/data/output.txt
+git push >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
 
 
 
 echo open-source-notes >> c:/data/output.txt
-cd 'C:\OneDriveSparta\OneDrive - Sparta Global Limited\notes\open-source-notes' >> c:/data/output.txt
+cd 'C:\GDrive\Notes\course-notes' >> c:/data/output.txt
 git pull >> c:/data/output.txt
 git status >> c:/data/output.txt
-subl c:/data/output.txt
+git add . >> c:/data/output.txt
+git commit -m "Updating open source notes" >> c:/data/output.txt
+git push >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
+echo ""; >> c:/data/output.txt
+
+
+
+
+subl c:/data/output.txt 
+
