40bcf3941e305f9e7fd4949f429c333e708efb9c
Phil <philanderson888@hotmail.com>
2019-12-17 13:28:14 +0000

    Updating scripts

---
+++ MAC/gitcheckWorkMAC.sh
@@ -0,0 +1,206 @@
+
+
+echo "";
+echo "Updating scripts"
+cd /Users/Shared/scripts
+git status
+git pull
+git add .
+git commit -m "Updating scripts"
+git push
+echo "";
+
+
+
+
+
+echo "";
+echo "updating c-sharp"
+cd /Users/Shared/c-sharp
+git status
+git pull
+git add .
+git commit -m "Updating c-sharp"
+git push
+echo "";
+
+
+
+
+
+
+echo "";
+echo "updating c-sharp-tests"
+cd /Users/Shared/c-sharp-tests
+git status
+git pull
+git add .
+git commit -m "Updating c-sharp-tests"
+git push
+echo "";
+
+
+
+
+
+
+echo "";
+echo "updating 2019-04-c-sharp-labs"
+cd /Users/Shared/2019-04-c-sharp-labs
+git status
+git pull
+git add .
+git commit -m "Updating 2019-04-c-sharp-labs"
+git push
+echo "";
+
+
+
+
+
+echo "";
+echo "updating 2019-06-c-sharp-labs"
+cd /Users/Shared/2019-06-c-sharp-labs
+git status
+git pull
+git add .
+git commit -m "Updating 2019-06-c-sharp-labs"
+git push
+echo "";
+
+
+
+
+
+echo "";
+echo "updating 2019-09-c-sharp-labs"
+cd /Users/Shared/2019-09-c-sharp-labs
+git status
+git pull
+git add .
+git commit -m "Updating 2019-09-c-sharp-labs"
+git push
+echo "";
+
+
+
+
+
+
+
+echo "";
+echo "updating 2019-11-c-sharp-labs"
+cd /Users/Shared/2019-11-c-sharp-labs
+git status
+git pull
+git add .
+git commit -m "Updating 2019-11-c-sharp-labs"
+git push
+echo "";
+
+
+
+
+
+
+echo "";
+echo "Updating blog"
+cd /Users/Shared/blog
+git status
+git pull
+git add .
+git commit -m "Updating contentful blog"
+git push
+echo "";
+
+
+
+
+
+echo "";
+echo "Updating bootstrap"
+cd /Users/Shared/bootstrap
+git status
+git pull
+git add .
+git commit -m "Updating bootstrap"
+git push
+echo "";
+
+
+
+
+
+
+
+echo "Updating bootstrap"
+cd /Users/Shared/bootstrap
+git status
+git pull
+git add .
+git commit -m "Updating bootstrap"
+git push
+echo "";
+
+
+
+
+
+
+
+echo "Updating /Users/apple/Google Drive/Notes/course-notes"
+cd "/Users/apple/Google Drive/Notes/course-notes"
+git status
+git pull
+git add .
+git commit -m "Updating course-notes"
+git push
+echo "";
+
+
+
+
+
+
+
+
+
+echo "Showcase"
+cd /Users/Shared/showcase
+git status
+git pull
+git add .
+git commit -m "Updating Showcase"
+git push
+echo "";
+
+
+
+
+
+
+echo "Updating vue"
+cd /Users/Shared/vue
+git status
+git pull
+git add .
+git commit -m "Updating Vue"
+git push
+echo "";
+
+
+
+
+
+
+
+echo "Updating vue-with-paypal-payment"
+cd /Users/Shared/vue-with-paypal-payment
+git status
+git pull
+git add .
+git commit -m "Updating Vue With Paypal Payment"
+git push
+echo "";
+
+
