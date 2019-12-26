d97169f61f87bbd73ef8452822bda6d561f0715e
philanderson888 <philanderson888@hotmail.com>
2019-12-26 06:26:39 +0000

    updating scripts

--- Windows/gitcheckapple.ps1
+++ Windows/gitcheckapple.ps1
@@ -34,4 +34,35 @@ echo "" >> c:/data/output.txt
 
 
 
+
+echo "All finished updating - now double checking there are no more changes to push through" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+echo c-sharp >> c:/data/output.txt
+cd C:\c-sharp >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+git add .
+git commit -m "Updating c-sharp"
+git push
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+
+
+echo scripts >> c:/data/output.txt
+cd C:\scripts >> c:/data/output.txt
+git pull >> c:/data/output.txt
+git status >> c:/data/output.txt
+git add .
+git commit -m "updating scripts"
+git push
+echo "" >> c:/data/output.txt
+echo "" >> c:/data/output.txt
+
+
+
 notepad C:\data\output.txt
