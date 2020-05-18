3efa59624fc16f9028eee2c1f8c90882cf2e3b8e
philanderson888 <philanderson888@hotmail.com>
2020-05-18 06:02:24 +0100

    Updating GitCheck script to automatically use list of folders as source for iteration

--- Windows/gitcheck.ps1
+++ Windows/gitcheck.ps1
@@ -9,7 +9,7 @@ Write-Output $output
 if ($env:COMPUTERNAME -eq "PHILIP-DESKTOP") {
     $output = "About to sync repos for machine " + $env:COMPUTERNAME
     Write-Output $output
-    $repositoryRootPath = "C:\github"
+    $repositoryRootPath = "C:\github\"
 }
 else{
     $repositoryRootPath = "C:\"
@@ -24,139 +24,51 @@ copy-item -Path c:\scripts\Windows\* -Destination c:\users\$env:USERNAME\Windows
 Write-Host ""
 Write-Host ""
 
-Write-Host "checking git diff for all repos first "
+Write-Host "checking git diff for the following GitHub repositories: "
+$ListOfGithubRepositories = Get-ChildItem -Path $repositoryRootPath -Directory
+
+Write-Host C:\scripts
+foreach($repository in $ListOfGithubRepositories){
+    Write-Host $repository
+}
+
+Write-Host ""
+Write-Host ""
 
 Write-Host C:\scripts 
 Set-Location C:\scripts 
 git status
 git diff
 
-
-$workingPath = $repositoryRootPath + "\2020-01-c-sharp-labs"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-$workingPath = $repositoryRootPath + "\blog"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-$workingPath = $repositoryRootPath + "\c-sharp"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-$workingPath = $repositoryRootPath + "\family-app"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-
-$workingPath = $repositoryRootPath + "\vue"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-
-$workingPath = $repositoryRootPath + "\vue-live"
-Write-Host $workingPath
-Set-Location $workingPath
-git status
-git diff
-
-
-
-if ($env:COMPUTERNAME -eq "") {
-    $output = "About to sync repos for machine " + $env:COMPUTERNAME
-    Write-Output $output
-
-    Write-Host C:\2019-04-c-sharp-labs 
-    cd C:\2019-04-c-sharp-labs 
+foreach($repository in $ListOfGithubRepositories){
+    Write-Host $repository
+    Write-Host '================='
+    Set-Location ($repositoryRootPath + $repository)
     git status
     git diff
-
-    Write-Host C:\2019-06-c-sharp-labs 
-    cd C:\2019-06-c-sharp-labs 
-    git status
-    git diff
-
-    Write-Host C:\2019-09-c-sharp-labs 
-    cd C:\2019-09-c-sharp-labs 
-    git status
-    git diff
-
-    Write-Host C:\2019-11-c-sharp-labs 
-    cd C:\2019-11-c-sharp-labs 
-    git status
-    git diff
-
-    Write-Host C:\2019-12_CSharpWalkthrough 
-    cd C:\2019-12_CSharpWalkthrough 
-    git status
-    git diff
-
-
-
-    Write-Host C:\bootstrap
-    cd C:\bootstrap
-    git status
-    git diff
-
-
-
-    Write-Host C:\c-sharp-tests 
-    cd C:\c-sharp-tests 
-    git status
-    git diff
-
-    Write-Host c:\course-notes
-    cd c:\course-notes
-    git status
-    git diff
-
-    Write-Host C:\cyber
-    cd C:\cyber 
-    git status
-    git diff
-
-
-
-    Write-Host C:\shells
-    cd C:\shells 
-    git status
-    git diff
-
-
-    Write-Host C:\showcase 
-    cd C:\showcase 
-    git status
-    git diff
-
-
-
-
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
 }
 
+Write-Host ""
+Write-Host ""
+Write-Host "Repository Check Complete"
+Write-Host "Press Enter To Continue Or Control-C To Cancel Push And Pull To GitHub"
+Write-Host ""
+Write-Host ""
+Write-Host ""
 
 
 Set-Location "c:\"
-
-
 pause
 
-
-
-
-
 Clear-Host
-
 Write-Host "Pushing and pulling github repos"
+
 
 
 Write-Host ""
@@ -171,287 +83,21 @@ Write-Host ""
 Write-Host "" 
 
 
-Write-Host ""
-Write-Host ""
-$workingPath = $repositoryRootPath + "\c-sharp"
-Write-Host $workingPath
-Set-Location $workingPath
-git pull 
-git add . 
-$commitMessage = "Updating " + $workingPath
-git commit -m $commitMessage
-git push 
-Write-Host "" 
-Write-Host "" 
 
-
-Write-Host ""
-Write-Host ""
-$workingPath = $repositoryRootPath + "\2020-01-c-sharp-labs"
-Write-Host $workingPath
-Set-Location $workingPath
-git pull 
-git add . 
-$commitMessage = "Updating " + $workingPath
-git commit -m $commitMessage
-git push 
-Write-Host "" 
-Write-Host "" 
-
-Write-Host ""
-Write-Host ""
-$workingPath = $repositoryRootPath + "\family-app"
-Write-Host $workingPath
-Set-Location $workingPath
-git pull 
-git add . 
-$commitMessage = "Updating " + $workingPath
-git commit -m $commitMessage
-git push 
-Write-Host "" 
-Write-Host "" 
-
-
-Write-Host ""
-Write-Host ""
-$workingPath = $repositoryRootPath + "\vue"
-Write-Host $workingPath
-Set-Location $workingPath
-git pull 
-git add . 
-$commitMessage = "Updating " + $workingPath
-git commit -m $commitMessage
-git push 
-Write-Host "" 
-Write-Host "" 
-
-
-Write-Host ""
-Write-Host ""
-$workingPath = $repositoryRootPath + "\vue-live"
-Write-Host $workingPath
-Set-Location $workingPath
-git pull 
-git add . 
-$commitMessage = "Updating " + $workingPath
-git commit -m $commitMessage
-git push 
-Write-Host "" 
-Write-Host "" 
-
-
-
-
-
-if ($env:COMPUTERNAME -eq "") {
-
-
-
-    Write-Host ""
-    Write-Host ""
-    Write-Host 2019-04-c-sharp-labs 
-    cd C:\2019-04-c-sharp-labs 
+foreach($repository in $ListOfGithubRepositories){
+    Write-Host $repository
+    Write-Host '================='
+    Set-Location ($repositoryRootPath + $repository)
     git pull 
     git add . 
-    git commit -m "Updating 2019-04-labs" 
+    $commitMessage = "Updating " + $workingPath
+    git commit -m $commitMessage
     git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
     Write-Host ""
     Write-Host ""
-    Write-Host 2019-06-c-sharp-labs 
-    cd C:\2019-06-c-sharp-labs 
-    git pull 
-    git add . 
-    git commit -m "Updating 2019-06-labs" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    
-    
     Write-Host ""
     Write-Host ""
-    Write-Host 2019-09-c-sharp-labs 
-    cd C:\2019-09-c-sharp-labs 
-    git pull 
-    git add . 
-    git commit -m "Updating 2019-09-labs" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host 2019-11-c-sharp-labs 
-    cd C:\2019-11-c-sharp-labs 
-    git pull 
-    git add . 
-    git commit -m "Updating 2019-11-labs" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host 2019-12_CSharpWalkthrough 
-    cd C:\2019-12_CSharpWalkthrough 
-    git pull 
-    git add . 
-    git commit -m "Updating CSharpWalkthrough" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host bootstrap 
-    cd C:\bootstrap 
-    git pull 
-    git add . 
-    git commit -m "Updating bootstrap" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host c-sharp-tests 
-    cd C:\c-sharp-tests 
-    git pull 
-    git add . 
-    git commit -m "Updating c-sharp-tests" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host course-notes 
-    cd c:\course-notes
-    git add . 
-    git commit -m "updating course-notes" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host cyber
-    cd c:\cyber
-    git add . 
-    git commit -m "updating cyber" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host shells
-    cd c:\shells
-    git add . 
-    git commit -m "updating shells" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
-    Write-Host ""
-    Write-Host ""
-    Write-Host showcase 
-    cd C:\showcase 
-    git pull 
-    git add . 
-    git commit -m "Updating showcase" 
-    git push 
-    Write-Host "" 
-    Write-Host "" 
-    
-    
-    
-    
-    
-    
 }
-
-
-
-
-
-
-
-
-
 
 
 
