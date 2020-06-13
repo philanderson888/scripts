46d86fb7bcfc0d847cc3e10ed4f5f33f3eb5cc03
philanderson888 <philanderson888@hotmail.com>
2020-06-13 06:52:22 +0100

    GitPushEvery20Minutes Add Personal Notes

--- Windows/gitpushevery20minutes.ps1
+++ Windows/gitpushevery20minutes.ps1
@@ -1,10 +1,10 @@
 function global:gitpushevery20minutes{
-    Set-Location /github/notes
+    Set-Location F:/GDrive/github/personal
     git pull 
     git add . 
     git commit -m "updating notes" 
     git push 
-    Set-Location f:/GDrive/github/course-notes 
+    Set-Location F:/GDrive/github/course-notes 
     git pull 
     git add . 
     git commit -m "updating notes" 
