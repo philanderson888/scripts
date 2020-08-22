592555d158176421110c54d8a598ecdf5d14521b
Phil Anderson <philanderson888@hotmail.com>
2020-08-22 08:50:40 +0100

    Install Windows Subsystem For Linux

---
+++ devops/install-windows-subsystem-for-linux.ps1
@@ -0,0 +1,3 @@
+# run this before docker works!
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart -WarningAction SilentlyContinue
+Restart-Computer -Force
