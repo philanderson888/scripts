f7d58452de5d6364df3554d685e7d8791bf2d81e
philanderson888 <philanderson888@hotmail.com>
2024-02-11 20:26:32 +0000

    aws console install vue using github repo

--- aws/awsLinux/script-10-run-vue-web-server.sh
+++ aws/awsLinux/script-10-run-vue-web-server.sh
@@ -3,47 +3,59 @@
 echo
 
 library=vue
-projectName = $library + 'WebServer'
-port=3010
+projectName=$library' web server'
+port=5173
 
 startTime="$(date +%s)"
 
-echo ==========================================================================
-echo ====================   creating $library project   =======================
-echo ==========================================================================
+echo =================================================================================
+echo ====================     $projectName - git clone project   =====================w
+echo =================================================================================
 
-echo creating vue@latest project
-npm create vue@latest $projectName
-cd $projectName
+echo creating vue@latest project - failing at present - avoiding this as failing 
+# npm create vue@latest $projectName
+# cd $projectName
+
+echo instead installing from github repo
+git clone https://github.com/philanderson888/vue
+cd vue/projects/starter-template-03
 #sed -i 's/3000/3008/g' server.js
 #sed -i 's/listening on port/node express server 08 responding on port/g' server.js
+
  
 echo ==============================================================================
-echo =================  run npm install to pull down libraries ====================
+echo ==================      $project - npm install                    ===================
 echo ==============================================================================
+
+echo install libraries for vue project
 npm install
 echo web server should now be installed
 
 
-echo ==============================================================================
-echo ===================        configure the server  ==============================
-echo ==============================================================================
-
-
-sed -i 's/8080/3010/g' server.js
-
-
 echo ============================================================================
-echo ==================     run $library web server          ====================
+echo ==================     $project - run web server          ====================
 echo ============================================================================
 
-echo starting $library web server
-npm run format
+echo starting $library web server on port $port
+pause 
+read -p
+npm run build
+read -p
 npm run dev
-
+read -p
 echo $library web server is running
+read -p
 
+
+echo ===============================================================================
+echo ===============        $project - test web server          ====================
+echo ===============================================================================
 echo test $library web server with local curl ipv4
 curl localhost:$port -s
 echo test $library web server with local curl ipv6
 curl ::1:$port -s
+echo $library web server curl testing complete
+echo
+echo
+echo
+echo
