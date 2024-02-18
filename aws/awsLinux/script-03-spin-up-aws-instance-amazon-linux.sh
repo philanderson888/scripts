c3a167c1b363a7a9fba6df74e3ab5ac7168d5f04
philanderson888 <philanderson888@hotmail.com>
2024-02-18 12:38:18 +0000

    get new json template files for aws instances

---
+++ aws/awsLinux/script-03-spin-up-aws-instance-amazon-linux.sh
@@ -0,0 +1,10 @@
+echo create new ec2 instance of amazon linux
+echo
+echo
+echo
+awsInstanceTemplatePath=file://script-03-spin-up-aws-instance-04-amazon-linux.json
+echo template used is $awsInstanceTemplatePath
+echo
+echo
+echo
+aws ec2 run-instances --cli-input-json $awsInstanceTemplatePath
--- aws/awsLinux/script-03-spin-up-aws-instance.sh
+++
@@ -1,3 +0,0 @@
-echo create new ec2 instance
-
-aws ec2 run-instances --cli-input-json file://script-03-spin-up-aws-instance.json
