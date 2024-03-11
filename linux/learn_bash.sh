f64dab6334033350a9e161cd35dc002f89707d7d
philanderson888 <philanderson888@hotmail.com>
2024-03-11 02:55:33 +0000

    learn bash tips

---
+++ linux/learn_bash.sh
@@ -0,0 +1,29 @@
+echo
+echo
+echo
+echo ==========================================
+echo =====   learn linux bash scripting   =====
+echo ==========================================
+echo
+echo
+echo
+echo create an array
+array=( one two three )
+echo
+echo
+echo
+echo print an array
+printf "%s\n" "${array[@]}"
+echo
+echo
+echo
+echo print individual items from an array
+printf "Group file in Linux or Unix: %s\n" "${array[1]}"
+echo
+echo
+echo
+echo iterate over array
+for item in "${!array[@]}"
+do
+  echo "array item is ... $item"
+done
