fe35ef0f685cec2e87cb18c4869c4884eeb38b87
philanderson888 <philanderson888@hotmail.com>
2024-03-30 05:59:52 +0000

    amend heading for script

--- aws/awsWindows/script-10-create-vm.sh
+++ aws/awsWindows/script-10-create-vm.sh
@@ -1,6 +1,6 @@
 echo "======================================================================="
-echo "====             create vm $vm_name using image $vm_image                 ===="
-echo "====                                           $minutes:$seconds                             ============="
+echo "====             create vm $vm_name using image $vm_image"
+printTime
 echo "======================================================================="
 az vm create --name $vm_name --resource-group $resource_group_name  --image $vm_image --admin-username $admin_username --admin-password $admin_password --ssh-key-value $ssh_key_public
 echo
