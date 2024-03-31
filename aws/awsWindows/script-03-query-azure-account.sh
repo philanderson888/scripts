fbb5f6fd566b098843d348a145c185419d3753a6
philanderson888 <philanderson888@hotmail.com>
2024-03-31 22:39:45 +0100

    amend headings

--- aws/awsWindows/script-03-query-azure-account.sh
+++ aws/awsWindows/script-03-query-azure-account.sh
@@ -1,14 +1,14 @@
-echo "===================================================================="
-echo "====                                     azure status               ============="
-echo "===================================================================="
+echo "============================================================="
+echo "====                  azure status                       ===="
+echo "============================================================="
 echo azure logged in account is ...
 az account show --query "user.name" --output tsv
 echo
 echo
 echo
-echo "===================================================================="
-echo "====                                  azure account list            ============="
-echo "===================================================================="
+echo "============================================================="
+echo "====                 azure account list                  ===="
+echo "============================================================="
 az account list --query "[].{name:name, cloudName:cloudName, subscriptionId:subscriptionId, tenantId:tenantId}" --output table
 echo
 echo
