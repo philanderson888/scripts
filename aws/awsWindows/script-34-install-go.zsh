38262543566199db8be7ef8c821afc85e96f3ffc
philanderson888 <philanderson888@hotmail.com>
2024-04-01 18:21:29 +0100

    upgrade install go shell script

--- aws/awsWindows/script-34-install-go.zsh
+++ aws/awsWindows/script-34-install-go.zsh
@@ -1,34 +1,78 @@
+#!/bin/bash
+bash
 echo "======================================================================="
-echo "====                             install go 2                               ===="
+echo "====                        install go "
 echo "======================================================================="
+echo list files
 ls
+echo list files and hidden files
 ls -lah
 echo install go using wget on linux
-wget --no-verbose -O - https://git.io/vQhTU | bash
+wget --quiet -O - https://git.io/vQhTU -o /dev/null | bash
+
 echo install go using curl on mac
 curl -L https://git.io/vQhTU | bash
+
+echo not sure what is happening here but I think i am making go available to bash
+source /home/azureuser/.bashrc
+
+
 echo "======================================================================="
-echo "====                          add go to PATH                               ===="
+echo "====                        add go to PATH                         ===="
 echo "======================================================================="
 export PATH=$PATH:/usr/local/go/bin
 source /home/azureuser/.bashrc
 echo "======================================================================="
-echo "====                             go version                                ===="
+echo "====                        go version                             ===="
 echo "======================================================================="
 go version
 echo "======================================================================="
-echo "====                          run   go version                                ===="
+echo "====                     run   go version                          ===="
 echo "======================================================================="
 echo list files in home directory
 ls
+echo
+echo
+echo
 echo move to go folder
 mv script-34.go go
 cd go
+echo
+echo
+echo 
 echo list files in go directory
 ls
-now can run the script-34.go file by calling it with the go command
-go run script-34.go
+echo
+echo
+echo
+echo now can run the script-34.go file by calling it with the go command
+go run ./script-34.go
+echo
+echo
+echo
 echo go build script-34.go will build hello-world file
-go build script-34.go
+go build ./script-34.go
 echo now can run the executable file called 'script-34'
 ./script-34
+
+
+echo "======================================================================="
+echo "====      upgrade ulimit so can log in with another shell          ===="
+echo "======================================================================="
+sudo bash ulimit -n 1000000
+echo "======================================================================="
+echo "====                      spawn another shell                      ===="
+echo "======================================================================="
+ssh -T localhost
+echo hello from a new shell 1
+echo ls
+ls
+go version
+
+
+
+ssh -tt
+echo hello from a new shell 2
+echo ls
+ls
+go version
