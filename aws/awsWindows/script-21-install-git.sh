c17eb639cba25c7f6d7f450ca8fb390701fa239a
philanderson888 <philanderson888@hotmail.com>
2024-04-01 18:23:38 +0100

    install git

--- aws/awsWindows/script-21-install-git.sh
+++ aws/awsWindows/script-21-install-git.sh
@@ -1,9 +1,9 @@
 if [[  "$os" == "$os_ubuntu" ]]; then
-    sudo apt-get install git -y
-    sudo apt-get install git-all -y
+    sudo apt-get -qq install git -y
+    #sudo apt-get -qq install git-all -y
 elif [[  "$os" == "$os_fedora" ]]; then
-    sudo dnf install git -y
-    sudo dnf install git-all -y
+    sudo dnf -qq install git -y
+    #sudo dnf -qq install git-all -y
 fi 
 git --version
 git_installed=true
