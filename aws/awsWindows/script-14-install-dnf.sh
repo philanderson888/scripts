0cc719b401cd163190f8eccb081fc48e9fa8066d
philanderson888 <philanderson888@hotmail.com>
2024-04-01 18:23:23 +0100

    do not install dnf on ubuntu but yes on fedora

--- aws/awsWindows/script-14-install-dnf.sh
+++ aws/awsWindows/script-14-install-dnf.sh
@@ -1,4 +1,68 @@
 #!/bin/bash
+#!/bin/bash
+echo "======================================================================="
+echo "====                          shell                                ===="
+echo "======================================================================="
+shell_version=$($SHELL --version)
+echo "shell in use on remote machine is $SHELL of version echo ${c_version:0:57}"
+echo "======================================================================="
+echo "====                        linux version                          ===="
+echo "======================================================================="
+os_type_debian=debian
+os_ubuntu=ubuntu
+os_centos=centos
+os_debian=debian
+echo "running a case statement on /etc/os-release against the option 'ID'"
+. /etc/os-release
+linux_release_id=not_recognised
+case "$ID" in
+    ubuntu) 
+        echo 'This is Ubuntu Linux' 
+        os=$os_ubuntu
+        ;;
+    centos)
+        echo 'This is centos Linux'
+        os=$os_centos
+        ;;
+    debian)
+        echo 'this is debian linux'
+        os=$os_debian
+        ;;
+    *)
+        echo 'This is an unknown linux distribution type' 
+        os=not_recognised
+        ;; 
+esac
+
+if [[  "$os" == "$os_ubuntu" ]]; then
+    os_type=$os_type_debian
+fi
+echo "=================================================================="
+echo "====              update $os of type $os_type 2"
+echo "=================================================================="
+if [[  "$os_type" == "$os_type_debian" ]]; then
+    echo "=============================================================="
+    echo "====              apt-get install nginx  2                 ===="
+    echo "=============================================================="
+    sudo apt-get install nginx -y >> output.txt
+    echo "=============================================================="
+    echo "====               update various services                ===="
+    echo "=============================================================="
+    sudo systemctl restart systemd-journald.service 
+    sudo /etc/needrestart/restart.d/systemd-manager
+    sudo systemctl restart systemd-networkd.service
+    sudo systemctl restart systemd-resolved.service
+    sudo systemctl restart systemd-logind.service
+    sudo systemctl restart unattended-upgrades.service
+elif [[  "$os_type" == "$os_type_fedora" ]]; then
+    echo confirmation of red hat release version 
+    echo "/etc/redhat-release"
+    echo /etc/redhat-release
+    echo "-f /etc/redhat-release"
+    echo -f /etc/redhat-release
+fi
+
+
 echo "=============================================================="
 echo "====                   install dnf                        ===="
 echo "=============================================================="
