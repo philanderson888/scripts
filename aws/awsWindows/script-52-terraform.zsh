43b77a6c8ea60e31a48cf4bfd0e351a8d1a23f7c
philanderson888 <philanderson888@hotmail.com>
2024-04-22 02:30:26 +0100

    update terraform script

--- aws/awsWindows/script-52-terraform.zsh
+++ aws/awsWindows/script-52-terraform.zsh
@@ -1,11 +1,29 @@
 #!/bin/zsh
 echo
+echo cd ~
+cd ~
+echo 
+echo
+echo
+echo mkdir terraform
+mkdir terraform
+echo
+echo
+echo
+echo mv ./script-52-terraform.tf terraform
+mv ./script-52-terraform.tf terraform
+echo
+echo
+echo
+echo cd terraform
+cd terraform
 echo
 echo
 echo terraform tutorials
 echo https://medium.com/@sanghpriya785/running-your-first-terraform-hello-world-example-4bd8bb5c3efc
 echo https://www.terraformbyexample.com/hello-world
 echo https://developer.hashicorp.com/terraform/install
+echo https://levelup.gitconnected.com/terraform-hello-world-b4985162cff2
 echo
 echo
 echo
@@ -26,3 +44,59 @@ echo
 echo
 echo sudo apt-get install terraform
 sudo apt-get -qq install terraform -y >> output.txt
+echo
+echo
+echo
+echo run terraform hello world
+echo terraform init a
+terraform init -input=false
+echo
+echo
+echo
+echo terraform init b
+terraform init -auto-approve
+echo
+echo
+echo
+echo ls
+ls
+echo
+echo
+echo
+echo terraform apply input false auto approve
+terraform apply -input=false -auto-approve 
+echo
+echo
+echo
+echo terraform apply input=false script-52-terraform.tf
+terraform apply -input=false script-52-terraform.tf
+echo
+echo
+echo
+echo create a plan with terraform plan out tfplan
+terraform plan -out=tfplan -input=false
+echo
+echo
+echo
+echo ls
+ls
+echo
+echo
+echo
+echo terraform apply tfplan
+terraform apply -input=false
+echo
+echo
+echo
+echo terraform apply -input=false -auto-approve -lock=false tfplan
+terraform apply -input=false -auto-approve -lock=false tfplan
+echo
+echo
+echo
+echo terraform plan -destroy -out=tfplan -input=false -lock=false
+terraform plan -destroy -out=tfplan -input=false -lock=false
+echo
+echo
+echo
+echo ls
+ls
