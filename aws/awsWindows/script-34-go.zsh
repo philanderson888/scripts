440baa2424f35996d1267d5f5da51886f1e12ab8
philanderson888 <philanderson888@hotmail.com>
2024-04-28 10:12:40 +0100

    troubleshoot go script which is not working

--- aws/awsWindows/script-34-go.zsh
+++ aws/awsWindows/script-34-go.zsh
@@ -1,72 +1,100 @@
-#!/bin/bash
-bash
-echo "======================================================================="
-echo "====                        install go "
-echo "======================================================================="
-echo list files
-ls
+#!/bin/zsh
+zsh
 echo
 echo
 echo
-echo install go using wget on linux
-wget --quiet -O - https://git.io/vQhTU -o /dev/null | bash
-
-#echo install go using curl on mac
-#curl -L https://git.io/vQhTU | bash
-echo source bashrc
-source /home/azureuser/.bashrc
-echo "======================================================================="
-echo "====                        add go to PATH                         ===="
-echo "======================================================================="
-export PATH=$PATH:/usr/local/go/bin
-source /home/azureuser/.bashrc
-echo "======================================================================="
-echo "====                        go version                             ===="
-echo "======================================================================="
-go version
-echo "======================================================================="
-echo "====                     run   go version                          ===="
-echo "======================================================================="
+echo cd ~
 cd ~
-echo move to go folder
+echo
+echo
+echo
+echo mkdir go
+mkdir go
+echo
+echo
+echom mv script-34.go go
 mv script-34.go go
+echo
+echo
+echo
+echo cd go
 cd go
 echo
 echo
 echo 
-echo list files in go directory
-ls
-echo
-echo
-echo
-echo now can run the script-34.go file by calling it with the go command
-go run ./script-34.go
-echo
-echo
-echo
-echo go build script-34.go will build hello-world file
-go build ./script-34.go
-echo now can run the executable file called 'script-34'
-./script-34
-
-
 echo "======================================================================="
-echo "====      upgrade ulimit so can log in with another shell          ===="
+echo "====                        install go 34-015"
 echo "======================================================================="
-sudo bash ulimit -n 1000000
-echo "======================================================================="
-echo "====                      spawn another shell                      ===="
-echo "======================================================================="
-ssh -T localhost
-echo hello from a new shell 1
-echo ls
-ls
-go version
-
-
-
-ssh -tt
-echo hello from a new shell 2
-echo ls
-ls
-go version
+#echo install go using wget on linux
+#wget --quiet -O - https://git.io/vQhTU -o /dev/null | bash
+echo
+echo
+echo
+echo install go using curl on mac
+curl -L https://git.io/vQhTU | bash
+if [[ $(command -v go) ]]; then
+    echo
+    echo
+    echo
+    echo source bashrc
+    source /home/azureuser/.bashrc
+    echo "======================================================================="
+    echo "====                        add go to PATH                         ===="
+    echo "======================================================================="
+    export PATH=$PATH:/usr/local/go/bin
+    source /home/azureuser/.bashrc
+    echo
+    echo
+    echo
+    echo golang is installed
+    echo
+    echo
+    echo
+    echo "======================================================================="
+    echo "====                        go version                             ===="
+    echo "======================================================================="
+    go version
+    echo "======================================================================="
+    echo "====                     run   go version                          ===="
+    echo "======================================================================="
+    echo now can run the script-34.go file by calling it with the go command
+    go run ./script-34.go
+    echo
+    echo
+    echo
+    echo go build script-34.go will build hello-world file
+    go build ./script-34.go
+    if [ -f ./script-34 ]; then
+        echo now execute script 34
+        ./script-34
+    fi
+    echo "======================================================================="
+    echo "====      upgrade ulimit so can log in with another shell          ===="
+    echo "======================================================================="
+    sudo bash ulimit -n 1000000
+    sleep 1
+    echo "======================================================================="
+    echo "====                spawn another shell 034200                     ===="
+    echo "======================================================================="
+    ssh -T localhost
+    echo hello from a new shell 1
+    echo ls
+    ls
+    go version
+    echo
+    echo
+    echo
+    ssh -tt
+    echo hello from a new shell 2
+    echo ls
+    ls
+    go version
+else
+    echo
+    echo
+    echo
+    echo go is not installed
+    echo
+    echo
+    echo
+fi
