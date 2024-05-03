b26a745d7c83a3b06f8a51fd073b1eaa5837e6b4
philanderson888 <philanderson888@hotmail.com>
2024-05-03 21:23:26 +0200

    add api fetch

--- aws/awsWindows/script-90-teaching.js
+++ aws/awsWindows/script-90-teaching.js
@@ -13,6 +13,63 @@ myArray.forEach( (item) => {
     console.log(item);
 });
 
+
+console.log('==================================================')
+console.log('====              fetch get                   ====')
+console.log('==================================================')
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+
+
+console.log('==================================================')
+console.log('====              fetch post                  ====')
+console.log('==================================================')      
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+
+
+
+
+
+console.log('==================================================')
+console.log('====               fetch put                  ====')
+console.log('==================================================')  
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+
+
+
+
+
 console.log('=======================================================')
 console.log('====                loops                          ====')
 console.log('====                every                          ====')
@@ -31,6 +88,9 @@ myEveryArray.every(function(item) {
     console.log(item);
     return true;    
 });
+
+
+
 console.log('==================================================')
 console.log('====         loop for ... of                  ====')
 console.log('==================================================')
