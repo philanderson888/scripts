5ac683c2efb4ef90f3469e1d54c750f7af0c851d
philanderson888 <philanderson888@hotmail.com>
2024-06-07 02:07:00 +0100

    rename node server

--- aws/awsWindows/script-41-launch-node-server.zsh
+++ aws/awsWindows/script-41-launch-node-server.zsh
@@ -1,8 +1,5 @@
-echo hi
-echo hi there
-echo hello
 echo "=============================================================="
-echo "====                 get resource group                   ===="
+echo "====       script 41 launch node web server               ===="
 echo "=============================================================="
 IFS=$'\n' resource_group_names=($(az group list --query [].name -o tsv))
 echo printing array
@@ -19,13 +16,8 @@ do
     fi
 done
 resource_group_name=$resource_group_valid_name
-echo "=============================================================="
-echo "====                     sign in                          ===="
-echo "=============================================================="
 ssh_key=~/.ssh/azureCliUbuntuLogin.pem
 admin_username=azureuser
 ubuntu_vm_name=ubuntuVm01
-echo aaaa
 public_ip_address=$(az vm show -d --resource-group $resource_group_name --name $ubuntu_vm_name --query publicIps -o tsv)
-echo bbbb
 ssh -i $ssh_key $admin_username@$public_ip_address 'zsh -s' < ./script-41-run-node.zsh
