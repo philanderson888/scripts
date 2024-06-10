4aaf1ace98dd2e081982de8e9bf0bfff452fad2a
philanderson888 <philanderson888@hotmail.com>
2024-06-10 20:34:41 +0100

    upgrade express libraries

--- aws/awsWindows/script-26-launch-express-02.sh
+++ aws/awsWindows/script-26-launch-express-02.sh
@@ -1,17 +1,10 @@
-echo "=============================================================="
-echo "====           launch node web server                     ===="
-echo "=============================================================="
+#!/bin/sh
 IFS=$'\n' resource_group_names=($(az group list --query [].name -o tsv))
-echo printing array
-printf '%s\n' "${resource_group_names[@]}"
 resource_group_valid_name=invalid
-echo for each over array
 for resource_group_index in "${!resource_group_names[@]}"
 do
     resource_group_name=${resource_group_names[$resource_group_index]}
-    echo $resource_group_name
     if [[ $resource_group_name = 'ResourceGroup'* ]]; then
-        echo found a resource group ... with name ... $resource_group_name
         resource_group_valid_name=$resource_group_name
     fi
 done
@@ -20,4 +13,5 @@ ssh_key=~/.ssh/azureCliUbuntuLogin.pem
 admin_username=azureuser
 ubuntu_vm_name=ubuntuVm01
 public_ip_address=$(az vm show -d --resource-group $resource_group_name --name $ubuntu_vm_name --query publicIps -o tsv)
-ssh -i $ssh_key $admin_username@$public_ip_address 'zsh -s' < ./script-26-express.zsh
+ssh -i $ssh_key $admin_username@$public_ip_address 'zsh -s' < script-26-run-express-02.sh
+
