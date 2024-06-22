8de8ecbb09a50755604e45d3c55b41ed69f503dc
philanderson888 <philanderson888@hotmail.com>
2024-06-22 21:25:55 +0100

    move server to a sample gitlab repo for download

---
+++ sample-apps/node/script-25-server.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3006;
+const server = http.createServer((req, res) => {
+  console.log('request recieved on node server port ' + port)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  output = `response from node js web server`
+  output += `http://${hostname}:${port}/\n`
+  res.end(output);
+});
+server.listen(port, hostname, () => {
+  console.log(`node js web server at http://${hostname}:${port}/`);
+});
