f74b11af067c773f501cf2a890986de714515b4f
philanderson888 <philanderson888@hotmail.com>
2024-06-22 21:29:44 +0100

    modify nginx install script

--- aws/awsWindows/script-23-install-nginx.sh
+++ aws/awsWindows/script-23-install-nginx.sh
@@ -1,13 +1,11 @@
 echo "======================================================================="
-echo "====                 apt-get install nginx 23"
-echo finish the job - here are some links to complete the job
+echo "====                    nginx"
+echo "======================================================================="
+echo nginx tutorials
 echo https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-20-04
 echo https://ubuntu.com/server/docs/how-to-install-nginx
-echo "======================================================================="
-sudo apt-get -qq install nginx -y
-echo
-echo
-echo
+sudo apt-get -qq install nginx -y > /dev/null
 sleep 1
-echo nginx version
+echo nginx -v
 nginx -v
+sleep 1
