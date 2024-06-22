7cca631b7bfa6fe4573c8c456981d444c6ff0fe8
philanderson888 <philanderson888@hotmail.com>
2024-06-22 21:28:35 +0100

    modify fish install scripts

--- aws/awsWindows/script-18-fish.zsh
+++ aws/awsWindows/script-18-fish.zsh
@@ -1,3 +1,8 @@
-sudo apt-add-repository ppa:fish-shell/release-3 -y > /dev/null
-sudo apt-get update -y > /dev/null
-sudo apt-get install fish -y > /dev/null
+echo "======================================================================="
+echo "====             apt-get install fish 04350                        ===="
+echo "======================================================================="
+sudo apt-add-repository ppa:fish-shell/release-3 -y  > /dev/null
+sudo apt-get -qq update -y > /dev/null
+sudo apt-get -qq install fish -y > /dev/null
+sleep 1
+fish --version
