3c65632be67c30b799f9f8d888383cd275e168ef
philanderson888 <philanderson888@hotmail.com>
2024-06-22 21:25:35 +0100

    modify script to test servers via curl once servers are live

--- aws/awsWindows/script-60-test-servers.zsh
+++ aws/awsWindows/script-60-test-servers.zsh
@@ -4,8 +4,6 @@ zsh
 echo cd ~
 cd ~
 ip=$public_ip_address
-echo now lets see summary of all web servers before we terminate the instance ...
-echo attempting to curl servers on various ports
 echo ip is $ip
 echo
 echo
@@ -20,37 +18,52 @@ curl $ip -s
 echo
 echo
 echo
-echo testing basic node web server on port 3006
-echo curl localhost:3006
-curl localhost:3006
-echo curl $ip:3006 -s
-curl $ip:3006 -s
+port=3006
+echo node server port $port
+echo curl localhost:$port
+curl localhost:$port
+echo curl $ip:$port -s
+curl $ip:$port -s
 echo
 echo
 echo
-echo server 07 bun
-curl $ip:3007 -s
+port=3007
+echo bun server port $port
+curl $ip:$port -s
 echo
 echo
 echo
 echo "======================================================================="
 echo "====                       curl ipv4   2                           ===="
 echo "======================================================================="
-curl localhost:3008 -s
+port=3008
+curl localhost:$port -s
+curl ::1:$port -s
+echo
+echo
+echo
 echo "======================================================================="
-echo "====                       curl ipv6    2                          ===="
+echo "====                    react - port 3009 3010                     ===="
 echo "======================================================================="
-curl ::1:3008 -s
+port=3009
+curl localhost:$port -s
+curl ::1:$port -s
+curl $ip:$port -s
+port=3010
+curl localhost:$port -s
+curl ::1:$port -s
+curl $ip:$port -s
+echo "======================================================================="
+echo "====                 vue - ports 3020 and 5173                     ===="
+echo "======================================================================="
+port=3020
+echo curl $port
+curl $ip:$port -s
 echo
 echo
 echo
-echo server 09 on react was too slow so not running it ...  
-echo
-echo
-echo
-echo server vue  
-curl $ip:3010 -s
-echo
-echo
-echo
-
+port=5173
+echo curl $port
+curl http://localhost:$port
+curl $ip:$port -s
+curl 127.0.0.1:$port -s
