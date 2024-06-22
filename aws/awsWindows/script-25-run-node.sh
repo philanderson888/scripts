5ed681fa78bd4011660ff6c005da1b5ccaf0f36c
philanderson888 <philanderson888@hotmail.com>
2024-06-22 21:34:34 +0100

    modify running of the node script

--- aws/awsWindows/script-25-run-node.sh
+++ aws/awsWindows/script-25-run-node.sh
@@ -1,34 +1,18 @@
 #!/bin/zsh
 zsh
-echo "=============================================================="
-echo "====              copy raw server files                   ===="
-echo "=============================================================="
+echo cd ~
 cd ~
 echo mkdir node25
 mkdir node25
-echo
-echo
-echo
-echo ls
-ls
-echo
-echo
-echo
-echo mv ./script-25-server.js node25
-mv ./script-25-server.js node25
-echo
-echo
-echo
-echo ls
-ls
-echo
-echo
-echo
+echo mv script-25-server.js node25
+mv script-25-server.js node25
 echo cd node25
 cd node25
+echo
+echo
+echo
 echo "=============================================================="
 echo "====              run node web server                     ===="
+echo "====             using node script-25-server.js           ===="
 echo "=============================================================="
-echo start web server
 node script-25-server.js
-echo end of script 25 node web server
