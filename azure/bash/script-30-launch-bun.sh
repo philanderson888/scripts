2610c97c1c45129046fb29634e20e5a4239b67af
philanderson888 <philanderson888@hotmail.com>
2024-06-29 04:19:37 +0100

    upgrade bun servers

--- azure/bash/script-30-launch-bun.sh
+++ azure/bash/script-30-launch-bun.sh
@@ -15,4 +15,4 @@ ssh_key=~/.ssh/azureCliUbuntuLogin.pem
 admin_username=azureuser
 ubuntu_vm_name=ubuntuVm01
 public_ip_address=$(az vm show -d --resource-group $resource_group_name --name $ubuntu_vm_name --query publicIps -o tsv)
-ssh -i $ssh_key $admin_username@$public_ip_address 'zsh -s' < ./script-30-bun.sh
+ssh -i $ssh_key $admin_username@$public_ip_address 'zsh -s' < ./script-30-run-bun.sh
--- azure/bash/script-30-run-bun.sh
+++ azure/bash/script-30-run-bun.sh
@@ -1,41 +1,19 @@
-
-#echo "======================================================================="
-#echo "====                     curl bun 25190                            ===="
-#echo "======================================================================="
-#cd ~
-#echo mkdir bun25190
-#mkdir bun25190
-#echo
-#echo
-#echo
-#echo cd bun25190
-#cd bun25190
-#echo
-#echo
-#echo
-#echo curl - f s S L https://bun.sh/install 
-#curl -fsSL https://bun.sh/install | bash
-#echo
-#echo
-#echo
-#touch ~/.bashrc
-#echo 'BUN_INSTALL="/home/azureuser/.bun"' >> ~/.bashrc
-#echo 'PATH="$BUN_INSTALL/bin:$PATH"' >> ~/.bashrc
-#echo
-#echo
-#echo
-#echo source ~/.bashrc
-#source ~/.bashrc
-#echo
-#echo
-#echo
-#echo not sure why bun is not installed 
-#echo bun v
-#bun -v
-#echo bun version
-#bun --version
-#node -e "console.log('bun version ' + Bun.version);"
-#node -e "console.log('bun version ' + Bun.revision);"
-#echo "======================================================================="
-#echo "====                 install bun complete 25190                    ===="
-#echo "======================================================================="
+#!/bin/zsh
+zsh
+echo find bun server
+echo cd ~/scripts/sample-apps/bun/bun-01-57329
+cd ~/scripts/sample-apps/bun/bun-01-57329
+echo
+echo
+echo
+echo npm install -g bun -y
+sudo npm install -g bun -y
+echo
+echo
+echo bun install
+bun install
+echo
+echo
+echo
+echo bun run dev
+bun run dev
