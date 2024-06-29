ae282e88e2c2e3e8a2dd8a91ea7ad02130b2af61
philanderson888 <philanderson888@hotmail.com>
2024-06-29 04:20:00 +0100

    upgrade react servers

---
+++ azure/bash/script-32-run-react-04.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+zsh
+echo find react server
+echo cd ~/scripts/sample-apps/bun/bun-react-01-58262
+cd ~/scripts/sample-apps/bun/bun-react-01-58262
+echo
+echo
+echo
+echo npm install bun -y
+npm install bun -y
+echo
+echo
+echo
+echo bun install
+bun install
+echo
+echo
+echo
+echo bun run dev
+bun run dev
