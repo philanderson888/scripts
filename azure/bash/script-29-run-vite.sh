9402112094f9301e527d3da0a8d614b08f984d9d
philanderson888 <philanderson888@hotmail.com>
2024-06-29 04:19:23 +0100

    upgrade vite servers

---
+++ azure/bash/script-29-run-vite.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+zsh
+echo find vite server
+echo cd ~/scripts/sample-apps/bun/bun-vite-54892
+cd ~/scripts/sample-apps/bun/bun-react-01-54892
+echo
+echo
+echo
+echo npm install bun -y
+npm install bun -y
+echo
+echo
+echo
+echo bun install
+bun install
+echo
+echo
+echo
+echo bun dev
+bun dev
