bab960c4e6b791ff4ec88cf39af964575beef64f
philanderson888 <philanderson888@hotmail.com>
2024-06-29 04:19:49 +0100

    create astro server

---
+++ azure/bash/script-31-run-astro.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+zsh
+echo find vite server
+echo cd ~/scripts/sample-apps/bun/bun-astro-39872
+cd ~/scripts/sample-apps/bun/bun-astro-39872
+echo
+echo
+echo
+echo npm install bun -y
+npm install bun -y
+echo
+echo
+echo
+echo bun install
+bun install
+echo
+echo
+echo
+echo bun run dev
+bun run dev
