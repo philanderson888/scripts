f5fc1e86fd399036e74b71187d8aa4bf56965099
philanderson888 <philanderson888@hotmail.com>
2024-07-06 01:10:02 +0100

    amend logging for create vm script

--- azure/bash/script-10-create-vm.sh
+++ azure/bash/script-10-create-vm.sh
@@ -1,8 +1,9 @@
+vm_name=$vm_01_name
 echo "======================================================================="
-echo "====             create vm $vm_01_name using image $vm_image"
+echo "====             create vm $vm_name using image $vm_image"
 printTime
 echo "======================================================================="
-az vm create --name $vm_01_name --resource-group $resource_group_name  --image $vm_image --admin-username $admin_username --admin-password $admin_password --ssh-key-value $ssh_key_public
+az vm create --name $vm_name --resource-group $resource_group_name  --image $vm_image --admin-username $admin_username --admin-password $admin_password --ssh-key-value $ssh_key_public
 echo
 echo
 echo
