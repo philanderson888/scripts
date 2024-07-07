9bc0841291fca2fa96054157368e3d9d792c9742
philanderson888 <philanderson888@hotmail.com>
2024-07-07 14:15:28 +0100

    amend folder names for react

--- aws/awsLinux/script-09-install-and-run-create-react-app.sh
+++ aws/awsLinux/script-09-install-and-run-create-react-app.sh
@@ -9,14 +9,14 @@ npm install -g npx
 echo
 echo
 echo
-echo  npm install yarn
+echo npm install yarn
 npm install --global yarn > /dev/null
 echo 
 echo
 echo
 echo yarn create react app react 01
 yarn create react-app react01
-cd react02
+cd react01
 echo
 echo
 echo
@@ -28,7 +28,3 @@ echo
 echo
 echo start react01 app on port $port
 npm start
-# for react 03 app
-#echo creating react app using npx which uses latest versions of the libraries
-#echo this takes too long so commenting out this line for now
-#npx create-react-app server05
