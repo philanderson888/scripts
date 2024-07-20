d5d5082556f6acff83664d3c8e9159743dee7cd1
philanderson888 <philanderson888@hotmail.com>
2024-07-20 09:48:31 +0100

    amend test server script to pull raw text from the internet

--- azure/bash/script-60-test-servers-from-server-2.sh
+++ azure/bash/script-60-test-servers-from-server-2.sh
@@ -23,6 +23,7 @@ echo
 echo
 vm_1_public_ip=$(cat vm_1_public_ip.txt)
 echo public ip of other server is $vm_1_public_ip
+echo ports are 80, 3000, 57329, 58262, 54892, 63892, 51547, 51279
 for port in 80 3000 57329 58262 54892 63892 51547 51279 
 do
     echo
@@ -38,3 +39,13 @@ do
     curl $vm_1_public_ip:$port
     sleep 3
 done
+
+rawTextUrl=https://raw.githubusercontent.com/philanderson888/scripts/master/sample-apps/text/sample.txt
+echo
+echo
+echo
+echo curl $rawTextUrl
+curl $rawTextUrl
+echo
+echo
+echo
