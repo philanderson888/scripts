367508b78d044bdfd341e7a046884db5ab2aab6b
philanderson888 <philanderson888@hotmail.com>
2024-11-02 04:47:43 +0000

    update script to attempt to run powershell on linux

--- .github/scripts/run-powershell-on-linux.sh
+++ .github/scripts/run-powershell-on-linux.sh
@@ -1,40 +1,21 @@
 # taken from https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.4
-
-# Update the list of packages
 sudo apt-get update
-
-# Install pre-requisite packages.
 sudo apt-get install -y wget apt-transport-https software-properties-common
-
 # Get the version of Ubuntu
 source /etc/os-release
-
-# Download the Microsoft repository keys
 wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
-
-# Register the Microsoft repository keys
 sudo dpkg -i packages-microsoft-prod.deb
-
-# Delete the Microsoft repository keys file
 rm packages-microsoft-prod.deb
-
-# Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
-
-###################################
-# Install PowerShell
 sudo apt-get install -y powershell
 
-# Start PowerShell
+echo " "
+echo "script to run powershell commands on linux ubuntu ... "
+
+echo " "
+echo "run powershell executable"
 pwsh
 
-echo "hello from the world of github scripting"
-echo "this is my first ever github cicd pipeline script"
-echo "run using github actions"
-echo "happy days"
-echo "using powershell on linux"
-echo "checking it is all working ... running Get-Command"
-Get-Command -Name Get-Command
 pwsh -Command {Get-Command -Name Get-Command}
 echo " "
 echo "Get-Process"
@@ -49,15 +30,34 @@ echo " "
 echo "Get-Location"
 pwsh -Command {Get-Location}
 echo " "
+pwsn -command Get-Command
+pwsh -command Get-Process
+pwsh -command Get-Service
+pwsh -command Get-Date
+pwsh -command Get-Location
 pwsh echo hello from powershell
 echo " "
 echo "now lets try running a powershell script file"
+echo " "
+echo pwd
 pwd
+echo " "
+echo ls
 ls
+
+echo " "
+echo "cd /github/workspace"
 cd /github/workspace
+echo ls
 ls
+
+echo " "
+echo "cd /"
 cd / 
+echo ls
 ls
+
+echo " "
 /powershell/hello-world.ps1
 pwsh /powershell/hello-world.ps1
 powershell /powershell/hello-world.ps1
