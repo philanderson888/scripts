a0c06f9220f4e9bb279a634b3ace4f442e07025a
philanderson888 <philanderson888@hotmail.com>
2024-11-03 06:33:42 +0000

    switch between bash and zsh, also add notes about the benefits of zsh

--- bash/2024-11-zsh.sh
+++ bash/2024-11-zsh.sh
@@ -18,16 +18,71 @@ echo " "
 
 
 echo "========================================"
+echo "              About"
+echo "========================================"
+echo " "
+
+echo " "
+echo "Zsh is a shell designed for interactive use, although it is also a powerful scripting language. "
+echo "Many of the useful features of bash, ksh, and tcsh were incorporated into zsh; many original features were added. "
+echo "The overall goal is to provide a more consistent and intuitive command set than is provided by other shells. "
+
+echo " "
+echo "zsh has features such as:"
+echo " "
+echo "1. Spell checking"
+echo "2. Shared history"
+echo "3. Auto-completion"
+echo "4. Prompt themes"
+echo "5. Directory jumping"
+echo "6. Customizable key bindings"
+echo "7. And more"
+
+
+echo "========================================"
 echo "              Install"
 echo "========================================"
 echo " "
 
 
 echo " "
+echo "sudo apt-get install zsh -y"
 sudo apt-get install zsh -y
 
 echo " "
 echo "check the version"
-echo " "
+echo "zsh --version"
 zsh --version
+
+
+
+echo "========================================"
+echo "               Use"
+echo "========================================"
+echo " "
+
+
+echo " "
+echo "we can switch between bash and zsh using `bash` and `zsh` commands"
+echo "echo $SHELL"
+echo $SHELL
+echo "zsh"
+zsh
+echo "echo $SHELL"
+echo $SHELL
+echo "bash"
+bash
+echo "echo $SHELL"
+echo $SHELL
+
+
+echo " "
+echo "we switch between bash and zsh using the command chsh -s /bin/zsh"
+echo "echo $SHELL"
+echo $SHELL
+echo "chsh -s /bin/zsh"
+chsh -s /bin/zsh
+echo "echo $SHELL"
+echo $SHELL
+
 
