2b6f2fbcf4d50458b4342519b53cf6c85b0b0403
philanderson888 <philanderson888@hotmail.com>
2024-11-03 13:50:06 +0000

    add bash script file

---
+++ bash/2024-11-bash.sh
@@ -0,0 +1,45 @@
+echo " "
+echo "========================================"
+echo "========================================"
+echo "========================================"
+echo "                 bash"
+echo "========================================"
+echo "========================================"
+echo "========================================"
+echo " "
+
+echo " "
+echo "========================================"
+echo "========================================"
+echo "        Section 1 - Learning"
+echo "========================================"
+echo "========================================"
+echo " "
+
+echo " "
+echo "1. What is bash?"
+echo "bash is a Unix shell and command language written by Brian Fox for the GNU Project as a free software replacement for the Bourne shell. "
+echo "First released in 1989, it has been distributed widely as the shell for the GNU operating system and as a default shell on Linux and macOS. "
+echo "It has been ported to Microsoft Windows and distributed with Cygwin and MinGW, to DOS by the DJGPP project,"
+echo "to Novell NetWare and to Android via various terminal emulation applications. "
+
+
+echo " "
+echo "========================================"
+echo "               Head"
+echo "========================================"
+
+echo " "
+echo "to print the first few lines of a file use head"
+head -10 ~/.bashrc
+
+
+echo " "
+echo "========================================"
+echo "               Tail"
+echo "========================================"
+
+echo " "
+echo "to print the last few lines of a file use tail"
+tail -10 ~/.bashrc
+
