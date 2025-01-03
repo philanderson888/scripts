008df341e52065d781fb8758a5e51bc8f6c7a76e
philanderson888 <philanderson888@hotmail.com>
2025-01-03 21:01:21 +0000

    add script to collect notes

---
+++ bash/2025-01-collect-notes.sh
@@ -0,0 +1,52 @@
+echo "\n\n\n\n\n"
+echo "========================================"
+echo "========================================"
+echo "====         collecting notes       ===="
+echo "========================================"
+echo "========================================"
+root_path=~/github
+echo "root_path=$root_path"
+delete_me=~/temp/delete-me.txt
+echo "delete_me=$delete_me"
+echo "========================================"
+echo " "
+echo "========================================"
+echo "====            clean up            ===="
+echo "========================================"
+rm $delete_me
+touch $delete_me
+echo " "
+echo " "
+echo " "
+echo "========================================"
+echo "====           planning             ===="
+echo "========================================"
+cat $root_path/personal-planning/2024-12-speak-to-me.sh >> $delete_me
+echo " "
+echo " "
+echo " "
+echo "========================================"
+echo "====          all learning          ===="
+echo "========================================"
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====           all learning         ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+cat $root_path/course-notes/random/all.md >> $delete_me
+echo " "
+echo " "
+echo " "
+echo "========================================"
+echo "====           c# / .NET            ===="
+echo "========================================"
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====           c# / .NET          ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+cat $root_path/c-sharp/README.md >> $delete_me
