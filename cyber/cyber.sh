e4aee27bc455822af11335146e8f533f3a9c468b
philanderson888 <philanderson888@hotmail.com>
2025-01-03 21:01:09 +0000

    add cyber script

---
+++ cyber/cyber.sh
@@ -0,0 +1,20 @@
+echo " "
+echo "========================================"
+echo "========================================"
+echo "====           cyber tools          ===="
+echo "========================================"
+echo "========================================"
+echo " "
+
+echo " "
+echo "========================================"
+echo "====              hping3            ===="
+echo "========================================"
+echo " "
+
+# this is linux
+# sudo apt-get install hping3
+
+# install mac ports if on mac
+echo best not to install this on mac - just install it on a raw linux machine built in the cloud
+sudo port install hping3 -y
