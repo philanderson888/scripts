a5ec7736e618ff5bea7508151059c572fb3bacb7
philanderson888 <philanderson888@hotmail.com>
2025-01-06 14:03:02 +0000

    update collect-notes script to include additional course and Rust project notes

--- bash/2025-01-collect-notes.sh
+++ bash/2025-01-collect-notes.sh
@@ -36,6 +36,50 @@ echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 cat $root_path/course-notes/random/all.md >> $delete_me
+cat $root_path/course-notes/random/2024-10-accounts.md >> $delete_me
+cat $root_path/course-notes/random/2024-11- >> $delete_me
+cat $root_path/course-notes/random/2024-worship-songs.md >> $delete_me
+cat $root_path/course-notes/random/2024-q2.md >> $delete_me
+cat $root_path/course-notes/random/2024-q3.md >> $delete_me
+cat $root_path/course-notes/random/2018.md >> $delete_me
+echo " "
+echo " "
+echo " "
+echo "========================================"
+echo "====               Rust             ===="
+echo "========================================"
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====               Rust             ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+cat $root_path/rust/README.md >> $delete_me
+cat $root_path/rust/notes/01-getting-started.md >> $delete_me
+cat $root_path/rust/notes/02-first-project.md >> $delete_me
+cat $root_path/rust/projects/02-guessing-game/guessing-game-05/src/main.rs >> $delete_me
+cat $root_path/rust/projects/03-variables-operators-functions/src/main.rs >> $delete_me
+cat $root_path/rust/projects/04-ownership/src/main.rs >> $delete_me
+cat $root_path/rust/projects/05-structs-and-enums/src/main.rs >> $delete_me
+cat $root_path/rust/projects/06-crates/src/main.rs >> $delete_me
+cat $root_path/rust/projects/07-collections/src/main.rs >> $delete_me
+cat $root_path/rust/projects/08-error-handling/src/main.rs >> $delete_me
+cat $root_path/rust/projects/10-generics/src/main.rs >> $delete_me
+cat $root_path/rust/projects/11-testing/src/main.rs >> $delete_me
+cat $root_path/rust/projects/12-grep/grep-11/src/main.rs >> $delete_me
+cat $root_path/rust/projects/13-functional-programming/src/main.rs >> $delete_me
+cat $root_path/rust/projects/14-release/src/main.rs >> $delete_me
+cat $root_path/rust/projects/16-threads/src/main.rs >> $delete_me
+cat $root_path/rust/projects/17-oop/src/main.rs >> $delete_me
+cat $root_path/rust/projects/18-environment-variables/src/main.rs >> $delete_me
+cat $root_path/rust/projects/19-file-handling/src/main.rs >> $delete_me
+cat $root_path/rust/projects/20-gui-hello-world/src/main.rs >> $delete_me
+cat $root_path/rust/projects/21-gui-app/gui-07/src/main.rs >> $delete_me
+cat $root_path/rust/projects/22-gui-listbox/src/main.rs >> $delete_me
+cat $root_path/rust/projects/guessing-game/guessing-game-05/src/main.rs >> $delete_me
+cat $root_path/rust/projects/release/
+
 echo " "
 echo " "
 echo " "
@@ -49,4 +93,5 @@ echo "====           c# / .NET          ====" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+cat $root_path/c-sharp/.................add in .net notes ......
 cat $root_path/c-sharp/README.md >> $delete_me
