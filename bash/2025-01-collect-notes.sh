96995ec97a9fd9ec66bc5854f4db0a2321d0e766
philanderson888 <philanderson888@hotmail.com>
2025-01-08 04:57:57 +0000

    enhance collect-notes script with additional output for planning and learning resources

--- bash/2025-01-collect-notes.sh
+++ bash/2025-01-collect-notes.sh
@@ -4,11 +4,16 @@ echo "========================================"
 echo "====         collecting notes       ===="
 echo "========================================"
 echo "========================================"
+echo " "
+echo " "
+echo "========================================"
+echo "====            root path           ===="
+echo "========================================"
 root_path=~/github
 echo "root_path=$root_path"
 delete_me=~/temp/delete-me.txt
 echo "delete_me=$delete_me"
-echo "========================================"
+echo " "
 echo " "
 echo "========================================"
 echo "====            clean up            ===="
@@ -17,23 +22,41 @@ rm $delete_me
 touch $delete_me
 echo " "
 echo " "
-echo " "
 echo "========================================"
 echo "====           planning             ===="
 echo "========================================"
+echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====        personal planning       ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo copy $root_path/personal-planning/2024-12-speak-to-me.sh
+echo copy $root_path/personal-planning/2024-12-speak-to-me.sh >> $delete_me
 cat $root_path/personal-planning/2024-12-speak-to-me.sh >> $delete_me
-echo " "
 echo " "
 echo " "
 echo "========================================"
 echo "====          all learning          ===="
 echo "========================================"
+echo copy $root_path/course-notes/random/all.md
+echo copy $root_path/course-notes/random/2024-10-accounts.md
+echo copy $root_path/course-notes/random/2024-worship-songs.md
+echo copy $root_path/course-notes/random/2024-q2.md
+echo copy $root_path/course-notes/random/2024-q3.md
+echo copy $root_path/course-notes/random/2018.md
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
 echo "====           all learning         ====" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
+echo copy $root_path/course-notes/random/all.md >> $delete_me
+echo copy $root_path/course-notes/random/2024-10-accounts.md >> $delete_me
+echo copy $root_path/course-notes/random/2024-worship-songs.md >> $delete_me
+echo copy $root_path/course-notes/random/2024-q2.md >> $delete_me
+echo copy $root_path/course-notes/random/2024-q3.md >> $delete_me
+echo copy $root_path/course-notes/random/2018.md >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 cat $root_path/course-notes/random/all.md >> $delete_me
 cat $root_path/course-notes/random/2024-10-accounts.md >> $delete_me
@@ -47,12 +70,52 @@ echo " "
 echo "========================================"
 echo "====               Rust             ===="
 echo "========================================"
+echo copy $root_path/rust/README.md
+echo copy $root_path/rust/notes/01-getting-started.md
+echo copy $root_path/rust/notes/02-first-project.md
+echo copy $root_path/rust/projects/02-guessing-game/guessing-game-05/src/main.rs
+echo copy $root_path/rust/projects/03-variables-operators-functions/src/main.rs
+echo copy $root_path/rust/projects/04-ownership/src/main.rs
+echo copy $root_path/rust/projects/05-structs-and-enums/src/main.rs
+echo copy $root_path/rust/projects/07-collections/src/main.rs
+echo copy $root_path/rust/projects/08-error-handling/src/main.rs
+echo copy $root_path/rust/projects/10-generics/src/main.rs
+echo copy $root_path/rust/projects/11-testing/testing-02/src/lib.rs
+echo copy $root_path/rust/projects/12-grep/grep-11/src/main.rs
+echo copy $root_path/rust/projects/13-functional-programming/src/main.rs
+echo copy $root_path/rust/projects/16-threads/src/main.rs
+echo copy $root_path/rust/projects/17-oop/src/main.rs
+echo copy $root_path/rust/projects/18-environment-variables/src/main.rs
+echo copy $root_path/rust/projects/19-file-handling/src/main.rs
+echo copy $root_path/rust/projects/20-gui-hello-world/src/main.rs
+echo copy $root_path/rust/projects/21-gui-app/gui-07/src/main.rs
+echo copy $root_path/rust/projects/22-gui-listbox/src/main.rs
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
 echo "====               Rust             ====" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
+echo copy $root_path/rust/README.md >> $delete_me
+echo copy $root_path/rust/notes/01-getting-started.md >> $delete_me
+echo copy $root_path/rust/notes/02-first-project.md >> $delete_me
+echo copy $root_path/rust/projects/02-guessing-game/guessing-game-05/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/03-variables-operators-functions/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/04-ownership/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/05-structs-and-enums/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/07-collections/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/08-error-handling/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/10-generics/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/11-testing/testing-02/src/lib.rs >> $delete_me
+echo copy $root_path/rust/projects/12-grep/grep-11/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/13-functional-programming/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/16-threads/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/17-oop/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/18-environment-variables/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/19-file-handling/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/20-gui-hello-world/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/21-gui-app/gui-07/src/main.rs >> $delete_me
+echo copy $root_path/rust/projects/22-gui-listbox/src/main.rs >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 cat $root_path/rust/README.md >> $delete_me
 cat $root_path/rust/notes/01-getting-started.md >> $delete_me
@@ -81,12 +144,37 @@ echo " "
 echo "========================================"
 echo "====           c# / .NET            ===="
 echo "========================================"
+echo copy $root_path/c-sharp/notes/01-dot-net-8-c-sharp-12.md
+echo copy $root_path/c-sharp/notes/03-types.md
+echo copy $root_path/c-sharp/notes/04-nullable-types.md
+echo copy $root_path/c-sharp/notes/20-win-ui.md
+echo copy $root_path/c-sharp/projects/01-overview/Program.cs
+echo copy $root_path/c-sharp/projects/03-types/Program.cs
+echo copy $root_path/c-sharp/projects/04-nullable-types/Program.cs
+echo copy $root_path/c-sharp/projects/10-tuples/Program.cs
+echo copy $root_path/c-sharp/README.md
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
 echo "====           c# / .NET          ====" >> $delete_me
 echo "========================================" >> $delete_me
 echo "========================================" >> $delete_me
+echo copy $root_path/c-sharp/notes/01-dot-net-8-c-sharp-12.md >> $delete_me
+echo copy $root_path/c-sharp/notes/03-types.md >> $delete_me
+echo copy $root_path/c-sharp/notes/04-nullable-types.md >> $delete_me
+echo copy $root_path/c-sharp/notes/20-win-ui.md >> $delete_me
+echo copy $root_path/c-sharp/projects/01-overview/Program.cs >> $delete_me
+echo copy $root_path/c-sharp/projects/03-types/Program.cs >> $delete_me
+echo copy $root_path/c-sharp/projects/04-nullable-types/Program.cs >> $delete_me
+echo copy $root_path/c-sharp/projects/10-tuples/Program.cs >> $delete_me
+echo copy $root_path/c-sharp/README.md >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-# cat $root_path/c-sharp/.................add in .net notes ......
+cat $root_path/c-sharp/notes/01-dot-net-8-c-sharp-12.md >> $delete_me
+cat $root_path/c-sharp/notes/03-types.md >> $delete_me
+cat $root_path/c-sharp/notes/04-nullable-types.md >> $delete_me
+cat $root_path/c-sharp/notes/20-win-ui.md >> $delete_me
+cat $root_path/c-sharp/projects/01-overview/Program.cs >> $delete_me
+cat $root_path/c-sharp/projects/03-types/Program.cs >> $delete_me
+cat $root_path/c-sharp/projects/04-nullable-types/Program.cs >> $delete_me
+cat $root_path/c-sharp/projects/10-tuples/Program.cs >> $delete_me
 cat $root_path/c-sharp/README.md >> $delete_me
