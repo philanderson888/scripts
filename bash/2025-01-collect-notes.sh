f1d162982618a25445e596f66b726582480de481
philanderson888 <philanderson888@hotmail.com>
2025-02-04 05:28:59 +0000

    refactor collect-notes script to improve formatting and organization of output sections

--- bash/2025-01-collect-notes.sh
+++ bash/2025-01-collect-notes.sh
@@ -22,6 +22,12 @@ rm $delete_me
 touch $delete_me
 echo " "
 echo " "
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================"
 echo "====           planning             ===="
 echo "========================================"
@@ -34,11 +40,21 @@ echo "========================================" >> $delete_me
 echo copy $root_path/personal-planning/2024-12-speak-to-me.sh
 echo copy $root_path/personal-planning/2024-12-speak-to-me.sh >> $delete_me
 cat $root_path/personal-planning/2024-12-speak-to-me.sh >> $delete_me
-echo " "
-echo " "
+echo "\n\n\n\n"
 echo "========================================"
 echo "====          all learning          ===="
 echo "========================================"
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====           all learning         ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
 echo copy $root_path/course-notes/random/all.md
 echo copy $root_path/course-notes/random/2024-10-accounts.md
 echo copy $root_path/course-notes/random/2024-worship-songs.md
@@ -60,13 +76,32 @@ echo copy $root_path/course-notes/random/2018.md >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 cat $root_path/course-notes/random/all.md >> $delete_me
 cat $root_path/course-notes/random/2024-10-accounts.md >> $delete_me
-cat $root_path/course-notes/random/2024-worship-songs.md >> $delete_me
-cat $root_path/course-notes/random/2024-q2.md >> $delete_me
-cat $root_path/course-notes/random/2024-q3.md >> $delete_me
 cat $root_path/course-notes/random/2018.md >> $delete_me
-echo " "
-echo " "
-echo " "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================"
 echo "====               Rust             ===="
 echo "========================================"
@@ -136,10 +171,37 @@ for FILENAME in $root_path/rust/projects/06-crates/*/*/*/*.rs; do
     cat $FILENAME >> $delete_me
 done
 
-echo " "            
-echo " "
-echo " "
-echo " "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================"
 echo "====           c# / .NET            ===="
 echo "========================================"
@@ -177,3 +239,21 @@ cat $root_path/c-sharp/projects/03-types/Program.cs >> $delete_me
 cat $root_path/c-sharp/projects/04-nullable-types/Program.cs >> $delete_me
 cat $root_path/c-sharp/projects/10-tuples/Program.cs >> $delete_me
 cat $root_path/c-sharp/README.md >> $delete_me
+
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+echo "========================================"
+echo "====        Pending deletion        ===="
+echo "====        A+/Net+/Sec+/Cyber      ===="
+echo "========================================"
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo "====        Pending deletion        ====" >> $delete_me
+echo "====        A+/Net+/Sec+/Cyber      ====" >> $delete_me
+echo "========================================" >> $delete_me
+echo "========================================" >> $delete_me
+echo copy $root_path/course-notes/random/all-bin.md
