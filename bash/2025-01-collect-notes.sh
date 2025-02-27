30f3cd856e512203125b5c51f4b24bcfebf7fa80
philanderson888 <philanderson888@hotmail.com>
2025-02-27 15:34:06 +0000

    update script to gather notes

--- bash/2025-01-collect-notes.sh
+++ bash/2025-01-collect-notes.sh
@@ -22,12 +22,6 @@ rm $delete_me
 touch $delete_me
 echo " "
 echo " "
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "========================================"
 echo "====           planning             ===="
 echo "========================================"
@@ -180,13 +174,41 @@ done
 
 
 
+# number of runs of this script
+run_counter=$(cat .script_data/run_counter)
+run_counter=$(( run_counter + 1 ))
+echo there have been $run_counter runs of this script
+echo store new value for next time
+echo $run_counter > .script_data/run_counter
 
+git_frequency=10
+git_counter=$(($run_counter % $git_frequency))
 
+echo running git every $git_frequency runs of this program - we are on run $git_counter ...
 
+if [ $git_counter -eq 0 ] ; then
+    
+    echo ... so git is run this time ... 
 
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
+    echo "========================================"
+    echo "====               Git              ===="
+    echo "========================================"
+    echo "========================================" >> $delete_me
+    echo "====              Git               ====" >> $delete_me
+    echo "========================================" >> $delete_me
+    echo copy $root_path/course-notes/programming/git.md
+    echo copy $root_path/course-notes/programming/git.md >> $delete_me
+    cat $root_path/course-notes/programming/git.md >> $delete_me
 
-
-
+else
+    echo ... so git is not run this time ...
+fi
 
 
 
@@ -239,6 +261,16 @@ cat $root_path/c-sharp/projects/03-types/Program.cs >> $delete_me
 cat $root_path/c-sharp/projects/04-nullable-types/Program.cs >> $delete_me
 cat $root_path/c-sharp/projects/10-tuples/Program.cs >> $delete_me
 cat $root_path/c-sharp/README.md >> $delete_me
+
+
+echo "songs on google - song index https://docs.google.com/spreadsheets/d/1ZWtHzGTwAVX6KYYc1XKJAj_t8RbUJlhOUs5NgJQHfEU/edit?gid=0#gid=0"
+echo "songs on google - song index https://docs.google.com/spreadsheets/d/1ZWtHzGTwAVX6KYYc1XKJAj_t8RbUJlhOUs5NgJQHfEU/edit?gid=0#gid=0" >> $delete_me
+
+echo "notion notes"
+echo "notion notes" >> $delete_me
+
+echo "https://www.notion.so/philanderson888/a4d7a66acde24f04a4ad44007fd22d56"
+echo "https://www.notion.so/philanderson888/a4d7a66acde24f04a4ad44007fd22d56" >> $delete_me
 
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" >> $delete_me
